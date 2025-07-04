9e04c061240139299068a12c3ecf54fb3919a525
nicolasss0312 <nicolas.cuervo.2014@gmail.com>
2025-07-04 17:45:23 -0500

    mi primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="quintorepo",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Nicolas Cuervo",                         # Tu nombre
+    author_email="nicolas.cuervo.2014@gmail.com",                 # Tu correo electrónico
+    url="https://github.com/nicolasss0312/quintorepo",     # URL del proyecto
+)
